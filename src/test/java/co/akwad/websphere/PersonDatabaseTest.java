4147fb1cf32fab423022cf6f81104e36906e939f
Ibraheam <iheemoo@gmail.com>
2018-02-11 10:20:26 +0300

    Refactored some resources

--- src/test/java/co/akwad/websphere/PersonDatabaseTest.java
+++ src/test/java/co/akwad/websphere/PersonDatabaseTest.java
@@ -5,38 +5,40 @@ import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.CoreMatchers.is;
 import javax.inject.Inject;
 
-import org.arquillian.cube.HostIp;
-import org.arquillian.cube.HostPort;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(Arquillian.class)
 public class PersonDatabaseTest {
-	
-//	@HostIp
-//	String ip;
-//	
-//	@HostPort(containerName = "websphere", value = 8080)
-//	int port;
 
-    @Deployment
-    public static WebArchive create() {
-        return ShrinkWrap.create(WebArchive.class)
-                .addClasses(Person.class, PersonDatabase.class)
-                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
-    }
+	@Deployment
+	public static WebArchive create() {
+		return ShrinkWrap.create(WebArchive.class)
+				.addClasses(Person.class, PersonDatabase.class)
+				.addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+	}
 
-    @Inject
-    PersonDatabase personDatabase;
+	// @Deployment
+	// public static JavaArchive create() {
+	// return ShrinkWrap.create(JavaArchive.class)
+	// .addClasses(Person.class, PersonDatabase.class)
+	// //.addPackages(false, Person.class.getPackage(),
+	// PersonDatabase.class.getPackage())
+	// .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+	// }
 
-    @Test
-    public void shouldGetPennyFromPersonDatabase() {
-        assertThat(personDatabase, notNullValue());
-        assertThat(personDatabase.getPerson(0).getName(), is("Penny"));
-    }
+	@Inject
+	PersonDatabase personDatabase;
+
+	@Test
+	public void shouldGetPennyFromPersonDatabase() {
+		assertThat(personDatabase, notNullValue());
+		assertThat(personDatabase.getPerson(0).getName(), is("Penny"));
+	}
 }
